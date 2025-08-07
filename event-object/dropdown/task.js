debb29c39a1f51a1ab8584e64727ebf90662e0dc
workplace <semenenko.sse@mail.ru>
2025-08-07 15:35:13 +0300

    second commit

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,4 +1,3 @@
-// Получаем все дропдауны на странице
 const dropdowns = document.querySelectorAll(".dropdown");
 
 dropdowns.forEach((dropdown) => {
@@ -6,26 +5,21 @@ dropdowns.forEach((dropdown) => {
   const listElem = dropdown.querySelector(".dropdown__list");
   const items = dropdown.querySelectorAll(".dropdown__item");
 
-  // Переключение списка (открыть/закрыть)
   valueElem.addEventListener("click", () => {
     listElem.classList.toggle("dropdown__list_active");
   });
 
-  // Обработка клика по пункту меню
   items.forEach((item) => {
     item.addEventListener("click", (e) => {
-      e.preventDefault(); // запрет перехода по ссылке
+      e.preventDefault(); 
 
-      // Записываем выбранное значение в .dropdown__value
       valueElem.textContent = item.textContent.trim();
 
-      // Закрываем список
       listElem.classList.remove("dropdown__list_active");
     });
   });
 });
 
-// Закрываем открытые списки при клике вне дропдауна
 document.addEventListener("click", (e) => {
   dropdowns.forEach((dropdown) => {
     if (!e.target.closest(".dropdown")) {
--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -17,14 +17,16 @@ class Game {
   }
 
   registerEvents() {
-    /*
-      TODO:
-      Написать обработчик события, который откликается
-      на каждый введённый символ.
-      В случае правильного ввода символа вызываем this.success()
-      При неправильном вводе символа - this.fail();
-      DOM-элемент текущего символа находится в свойстве this.currentSymbol.
-     */
+    document.addEventListener('keydown', (event) => {
+      const typedChar = event.key.toLowerCase();
+      const currentChar = this.currentSymbol.textContent.toLowerCase();
+  
+      if (typedChar === currentChar) {
+        this.success();
+      } else {
+        this.fail();
+      }
+    });
   }
 
   success() {
